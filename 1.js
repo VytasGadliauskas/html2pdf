039076b6bde331dd26faf817e4914865c36e8eb9
Testas <testas@testas.lt>
2022-03-24 14:52:32 +0200

    1

---
+++ 1.js
@@ -0,0 +1,13 @@
+function spauzdinti() {
+    const printContents = document.getElementById('saskaita').innerHTML;
+    const originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+
+}
+
+
+function eksportuotiPDF() {
+
+}
