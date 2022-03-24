a39be73bab7a99971e0a38606425a9bf635e12ab
Vytas Gadliauskas <vytasgadliauskas@gmail.com>
2022-03-24 21:43:45 +0200

    1

--- 1.js
+++ 1.js
@@ -1,11 +1,10 @@
-import { jsPDF } from "jspdf.umd.min.js";
+/*import { jsPDF  } from "jspdf";
 var doc = new jsPDF();
 
 function saugotiPDF() {
     doc.fromHTML(`<html><head><title>Saskaita</title></head><body>` + document.getElementById("saskaita").innerHTML + `</body></html>`);
     doc.save('saskaita.pdf');
-}
-
+} */ 
 
 
 function spausdinti() {
